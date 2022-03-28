2cbd29527845d7889f42af32b316ec2211a45320
Denikaev Emil <edenikaev@mail.ru>
2022-03-29 01:37:51 +0400

    Чистое приложение

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(
+    <div>
+      It's work
+    </div>,
+  document.getElementById('root')
+);
+
