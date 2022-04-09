95e6ba1712c56e3556705086640b058f2179d198
Denikaev Emil <edenikaev@mail.ru>
2022-04-09 22:40:00 +0400

    Модели данных и связи между ними

--- server/index.js
+++ server/index.js
@@ -1,9 +1,14 @@
 require('dotenv').config()
 const express = require('express')
 const sequelize = require('./db')
+const models = require('./models/models')
 const PORT = process.env.PORT || 5000
+const cors = require('cors')
+
 
 const app = express()
+app.use(cors())
+app.use(express.json())
 
 const start = async () => {
   try {
